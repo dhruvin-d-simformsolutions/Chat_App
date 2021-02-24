ae02be7cb281bef043264758c46cba9d9a6eca58
dhruvin-d-simformsolutions <dhruvin.d@simformsolutions.com>
2021-02-24 17:28:02 +0530

    Tutorial-socket_io Complete

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,79 +1,89 @@
-const scoket = io()
+const socket = io()
 
+// Elements
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
-const $locationButton = document.querySelector('#send-location')
+const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
-//Template
+// Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
-scoket.on('message', (message) => {
-    console.log(message);
+socket.on('message', (message) => {
+    console.log(message)
     const html = Mustache.render(messageTemplate, {
-        message : message.text,
-        createdAt : moment(message.createdAt).format('h:mm a')
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
-
 })
 
-scoket.on('locationMessage', (message)=>{
-    // console.log(url);
-    const html = Mustache.render(locationMessageTemplate    , {
-        url : message.url,
-        createdAt : moment(message.createdAt).format('h:mm a')
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
-    
 })
 
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
 
-document.querySelector("#message-form").addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-
-    //disable on submitting form
     $messageFormButton.setAttribute('disabled', 'disabled')
 
-
     const message = e.target.elements.message.value
-    scoket.emit("sendMessage", message, (error) => {
-        // console.log("Meassage ",message);
-        // enable
-        $messageFormButton.removeAttribute('disabled')
 
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
         $messageFormInput.value = ''
         $messageFormInput.focus()
+
         if (error) {
-            return console.log(error);
+            return console.log(error)
         }
-        console.log("Messge Delivered! ");
+
+        console.log('Message delivered!')
     })
 })
 
-
-$locationButton.addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
-        return alert('Geolocation is not supported by your browser')
+        return alert('Geolocation is not supported by your browser.')
     }
 
-    //disable
-    $locationButton.setAttribute('disabled', 'disabled')
+    $sendLocationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
-        //enable
-        $locationButton.removeAttribute('disabled')
-
-        //   console.log(position);
-        scoket.emit('sendLocation', {
+        socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
-            console.log('Location Shared');
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared!')  
         })
     })
+})
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
 })
