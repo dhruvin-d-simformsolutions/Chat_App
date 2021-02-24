ad6062fb374ab6c252b69ec19109c95c14d6f91e
dhruvin-d-simformsolutions <dhruvin.d@simformsolutions.com>
2021-02-24 16:23:21 +0530

    Rendering Messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,39 @@
 const scoket = io()
 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+//Template
+ const messageTemplate = document.querySelector('#message-template').innerHTML
+
+ scoket.on('message', (message) => {
+     console.log(message);
+    const html = Mustache.render(messageTemplate,{
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
+     
+ })
+ 
 document.querySelector("#message-form").addEventListener('submit', (e) => {
     e.preventDefault()
-
+    
+    
+    //disable on submitting form
+    $messageFormButton.setAttribute('disabled','disabled')
+    
+    
     const message = e.target.elements.message.value
     scoket.emit("sendMessage", message,(error)=>{
         // console.log("Meassage ",message);
+        // enable
+        $messageFormButton.removeAttribute('disabled')
+        
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
         if(error){
             return console.log(error);
         }
@@ -13,15 +41,19 @@ document.querySelector("#message-form").addEventListener('submit', (e) => {
     })
 })
 
-scoket.on('message', (welcommessage) => {
-    console.log(welcommessage);
-})
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$locationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+    
+    //disable
+    $locationButton.setAttribute('disabled','disabled')
+    
     navigator.geolocation.getCurrentPosition((position) => {
+        //enable
+        $locationButton.removeAttribute('disabled')
+        
         //   console.log(position);
         scoket.emit('sendLocation', {
             latitude: position.coords.latitude,
